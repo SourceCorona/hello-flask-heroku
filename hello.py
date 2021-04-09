1510cfc95f8df1a6be137c775be46d91e9876ea9
dhurgham <31870789+SourceCorona@users.noreply.github.com>
2021-04-09 05:49:30 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,request
+import random,string
+from gtts import gTTS
 app = Flask(__name__)
+class speech:
+    def __init__(self,text,lang):
+        self.text = text
+        self.lang = lang
+    def say(text,lang):
+        myobj = gTTS(text=text, lang=lang, slow=False)
+        if myobj.save(rand + ".mp3") != False:
+            return ("saved as  " + rand + ".mp3")
+        else:
+            return ("error")
 
-@app.route('/')
+@app.route("/", methods=['GET'])
+text = request.args.get("text")
+lang = request.args.get("lang")
 def hello_world():
-    return 'Hello World!'
+    return speech.say(text,lang)
 
 if __name__ == '__main__':
     app.run()
