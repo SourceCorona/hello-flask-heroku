0d21d778a368bd24915bf94c95dbdc672cd2ded6
dhurgham <31870789+SourceCorona@users.noreply.github.com>
2021-04-10 00:42:10 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-from flask import Flask,request
-import random,string,os
+from flask import Flask, request
+import random, string, os
 from gtts import gTTS
+
+
 def random_string(y):
     return ''.join(random.choice(string.ascii_letters) for x in range(y))
-rand = random_string(9)
 
 app = Flask(__name__)
-       
+
+
 @app.route("/", methods=['GET'])
 def hello_world():
+    rand = random_string(9)
     text = request.args.get("text")
     lang = request.args.get("lang")
     myobj = gTTS(text=text, lang=lang, slow=False)
@@ -16,6 +19,7 @@ def hello_world():
         return ("saved as  " + rand + ".mp3")
     else:
         return ("error")
+
 
 if __name__ == '__main__':
     app.run()
