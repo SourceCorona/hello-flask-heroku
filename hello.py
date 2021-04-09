d4d72701dd72c17be3508400a30f28fb86f50173
dhurgham <31870789+SourceCorona@users.noreply.github.com>
2021-04-09 06:07:49 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,25 +4,9 @@ from gtts import gTTS
 def random_string(y):
     return ''.join(random.choice(string.ascii_letters) for x in range(y))
 rand = random_string(9)
-# def say(text,lang):
-#     myobj = gTTS(text=text, lang=lang, slow=False)
-#     if myobj.save(rand + ".mp3") != False:
-#         return ("saved as  " + rand + ".mp3")
-#      else:
-#          return ("error")
-def glob(path):
-    files = []
-    for dentry in os.scandir(path):
-        files.append(dentry.name)
-    return (files)
 
 app = Flask(__name__)
-@app.route("/", methods=['GET'])
-def p_glob():
-    see = request.args.get("see")
-    if see:
-        return print(glob(see))
-        
+       
 @app.route("/", methods=['GET'])
 def hello_world():
     text = request.args.get("text")
