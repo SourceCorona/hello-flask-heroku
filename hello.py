2bb18cbcc7e754379c3b6c02b7ae47c6caffbbe3
dhurgham <31870789+SourceCorona@users.noreply.github.com>
2021-04-09 05:50:52 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask,request
 import random,string
 from gtts import gTTS
+def random_string(y):
+    return ''.join(random.choice(string.ascii_letters) for x in range(y))
+rand = random_string(9)
 app = Flask(__name__)
 class speech:
     def __init__(self,text,lang):
@@ -14,9 +17,9 @@ class speech:
             return ("error")
 
 @app.route("/", methods=['GET'])
-text = request.args.get("text")
-lang = request.args.get("lang")
 def hello_world():
+    text = request.args.get("text")
+    lang = request.args.get("lang")
     return speech.say(text,lang)
 
 if __name__ == '__main__':
